66936234501876605ca14d809d166b1808b3f421
LeoGui <leoguilpain@hotmail.fr>
2018-03-23 09:35:02 +0100

    code java leo

---
+++ src/corenlp/InsertApp.java
@@ -0,0 +1,86 @@
+package corenlp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+public class InsertApp {
+	
+	public static void main(String[] args) {
+
+		InsertApp app = new InsertApp();
+		// insert three new rows
+		app.insertAllumer("on","switch");
+		app.insertAllumer("on","activate");
+		app.insertAllumer("on","energize");
+		app.insertAllumer("on","ignite");
+		app.insertAllumer("on","kick-start");
+		//app.insertAllumer("put on");
+		app.insertAllumer("on","start");
+		
+		app.insertEteindre("off","unplug");
+		app.insertEteindre("off","shut");
+		app.insertEteindre("off","close");
+		app.insertEteindre("off","cut");
+		app.insertEteindre("off","kill");
+		app.insertEteindre("off","halt");
+		app.insertEteindre("off","douse");
+		app.insertEteindre("off","down");
+		
+	}
+
+	/**
+	 * Connect to the test.db database
+	 *
+	 * @return the Connection object
+	 */
+	private Connection connect() {
+		// SQLite connection string
+		String url = "jdbc:sqlite:/home/leo/eclipse-workspace/corenlp/corenlp";
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+
+	/**
+	 * Insert a new row into the warehouses table
+	 *
+	 * @param name
+	 * @param capacity
+	 */
+	public void insertAllumer(String name,String verb) {
+		String sql = "INSERT INTO verb(name,verb) VALUES(?,?)";
+
+		try (Connection conn = this.connect();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, name);
+			pstmt.setString(2, verb);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void insertEteindre(String name,String verb) {
+		String sql = "INSERT INTO verb(name,verb) VALUES(?,?)";
+
+		try (Connection conn = this.connect();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, name);
+			pstmt.setString(2, verb);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+
+
+}
---
+++ src/corenlp/SelectApp.java
@@ -0,0 +1,69 @@
+package corenlp;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class SelectApp {
+	
+	public static String verbe;
+	public static String [] tab = new String [100];
+	public BasicPipelineExample test;
+ 
+	public SelectApp(BasicPipelineExample test) {
+		this.test = test;
+		//tab = test.tabverb;
+	}
+	
+    public static void main(String[] args) {
+
+    }
+    
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(String verbe){
+    	String url = "jdbc:sqlite:/home/leo/eclipse-workspace/corenlp/corenlp";
+
+		String sql = "SELECT name FROM verb WHERE verb = " + verbe + ";";
+
+
+		try (
+			Connection conn = DriverManager.getConnection(url);
+			PreparedStatement pstmt  = conn.prepareStatement(sql)){
+//				int i =0;
+//				System.out.println(tab[i]);
+//			while(i!=tab.length) {
+//				verbe = tab[i];
+//				// set the value
+				//stmt.setString(1,verbe);
+				
+				ResultSet rs    = pstmt.executeQuery();
+				
+				// loop through the result set
+				while (rs.next()) {
+					System.out.println("Le verbe " + verbe + " correspond à la commande " + rs.getString("name") + "\t");
+				}
+				//i++;
+			//} 
+		}
+		
+			catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+
+ 
+}
